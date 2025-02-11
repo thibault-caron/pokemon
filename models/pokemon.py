22474c28b2fc3cc40544ab1b597fbb4f1aba666d
Thibault Caron <thibault.caron@laplateforme.io>
2025-02-11 14:36:02 +0100

    refactor: add json with type multiplicator, level_up and evolve

--- models/pokemon.py
+++ models/pokemon.py
@@ -1,19 +1,18 @@
 class Pokemon:    
-    def __init__(self, name, level, xp, types, hp, attack, savage=True, evolves_into=None):
+    def __init__(self, name, level, types, hp, attack, evolves_into=None):
         self.__name = name
         self.__level = level
-        self.__xp = xp
+        self.__xp = 0
         self.__types = types
         self.__hp = hp
         self.__attack = attack
-        self.__savage = savage
+        self.__savage = True
         self.__evolves_into = evolves_into
     
     # Getters
     def get_name(self):
         return self.__name
     
-
     def get_level(self):
         return self.__level
     
@@ -63,12 +62,22 @@ class Pokemon:
 
     def evolve(self):
         if self.__evolves_into:
-            self.__name = self.__evolves_into
-            print(f"{self.__name} has evolved!")
+            if self.__level >= self.__evolves_into[0]:
+                self.__name = self.__evolves_into[1]
+                self.__types = None  # recuperer les données de l'evolution dans 'pokemon.json'
+                self.__hp = None
+                self.__attack = None
+                self.__evolves_into = None
+                print(f"{self.__name} has evolved!")
         else:
             print(f"{self.__name} cannot evolve.")
-    
 
+    def level_up(self):
+        if self.__xp >= self.__level * 4:
+            self.__level += 1
+            print(f"{self.__name} has grown to level {self.__level} !")
+            self.__xp = 0
+    
     def display_info(self):
         """Display the Pokémon's details"""
         print(f"Name: {self.__name}")
