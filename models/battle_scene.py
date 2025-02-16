4232e9f69f606fdd075e5f9e52c1f18e412b40ef
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-02-16 20:26:08 +0100

    feat: add first battle version in battle scene

--- models/battle_scene.py
+++ models/battle_scene.py
@@ -3,16 +3,18 @@ import os
 from .config import *
 from .game_state import GameState
 from .button import Button
-from.display_pokemon import DisplayPokemon
+from .pokemon import Pokemon
+from .battle import Battle
 
 class BattleScene(GameState):
     def __init__(self, app):
         super().__init__(app, img_folder=os.path.join(os.getcwd(), "assets", "images"), file="battle_background.webp")
         self.caption = "Battle"
-        self.pokemon_cards = [
-            DisplayPokemon("Pikachu", 100, 100, 400, 200, app=self.app),
-            DisplayPokemon("Bulbasaur", 700, 100, 400, 200, app=self.app)
-        ]
+
+        self.player_pokemon = Pokemon("Pikachu", 5)
+        self.wild_pokemon = Pokemon("Caterpie", 1)
+        self.battle = Battle(self.player_pokemon, self.wild_pokemon)
+
         self.button1 = Button(50, 650, 200, 50, "Attack", self.attack)
         self.button2 = Button(450, 650, 300, 50, "Change Pokemon", self.change_pokemon)
         self.button3 = Button(950, 650, 200, 50, "Run", self.run)
@@ -20,7 +22,9 @@ class BattleScene(GameState):
 
     def attack(self):
         """ Attack the enemy. """
-        pass
+        victory = self.battle.turn()
+        if isinstance(victory, str):
+            exit()
 
     def change_pokemon(self):
         """ Change pokemon in the battle. """
@@ -30,15 +34,30 @@ class BattleScene(GameState):
         """ Escape from the battle. """
         pass
 
+    def draw_image(self, image_path, x, y):
+        """ Allow to draw a sprite """
+        try:
+            image = pygame.image.load(image_path)
+            image = pygame.transform.scale(image, (400, 400))
+            self.app.screen.blit(image, (x, y))
+        except pygame.error as error:
+            print(f"Error: there is no image at the path {image_path}: {error}")
+
+    def draw_player_pokemon_sprite(self, x, y):
+        """"""
+        self.draw_image(self.player_pokemon.get_front_sprite(), x, y)
+
+    def draw_wild_pokemon_sprite(self, x, y):
+        """"""
+        self.draw_image(self.wild_pokemon.get_front_sprite(), x, y)
+
     def draw(self):
         """Draw welcome menu scene"""
         super().draw()  # Draw background
+        self.draw_player_pokemon_sprite(100, 600)
+        self.draw_wild_pokemon_sprite(1100, 100)
 
         for button in self.buttons:
             button.process()  # Show buttons
 
-        if hasattr(self.app, 'screen'):
-            for pokemon_card in self.pokemon_cards:
-                pokemon_card.draw_card()
-        else:
-            print("Erreur: `self.app` ne contient pas d'attribut `screen`.")
+
